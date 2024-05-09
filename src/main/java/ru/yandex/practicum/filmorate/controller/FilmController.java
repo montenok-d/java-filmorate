c67f51c3efe6aaef62db20bff9b6ffabdd5df049
montenok <montenok-d@yandex.ru>
2024-05-09 16:09:03 +0300

    Add Film and User controllers

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,12 +1,17 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import jakarta.validation.Valid;
+import jakarta.validation.ValidationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
 
+import java.time.LocalDate;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("/films")
 public class FilmController {
@@ -19,16 +24,26 @@ public class FilmController {
     }
 
     @PostMapping
-    public Film create(@RequestBody Film film) {
-        film.setId(getNextId());
-        films.put(film.getId(), film);
+    public Film create(@Valid @RequestBody Film film) {
+        if (validateDate(film)) {
+            film.setId(getNextId());
+            films.put(film.getId(), film);
+            log.info("Saved film: " + film);
+        } else {
+            throw new ValidationException("Can't create the film");
+        }
         return film;
     }
 
     @PutMapping
-    public Film update(@RequestBody Film newFilm) {
+    public Film update(@Valid @RequestBody Film newFilm) {
         if (films.containsKey(newFilm.getId())) {
-            films.put(newFilm.getId(), newFilm);
+            if (validateDate(newFilm)) {
+                films.put(newFilm.getId(), newFilm);
+                log.info("Updated film: " + newFilm);
+            }
+        } else {
+            throw new ValidationException("Can't update the film");
         }
         return newFilm;
     }
@@ -40,5 +55,10 @@ public class FilmController {
                 .max()
                 .orElse(0);
         return ++currentMaxId;
+    }
+
+    private boolean validateDate(Film film) {
+        LocalDate releaseDate = film.getReleaseDate();
+        return releaseDate.isAfter(LocalDate.of(1895, 12, 28));
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import jakarta.validation.Valid;
+import jakarta.validation.ValidationException;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
 
@@ -19,16 +21,19 @@ public class UserController {
     }
 
     @PostMapping
-    public User create(@RequestBody User user) {
+    public User create(@Valid @RequestBody User user) {
         user.setId(getNextId());
+        validateName(user);
         users.put(user.getId(), user);
         return user;
     }
 
     @PutMapping
-    public User update(@RequestBody User user) {
-        if(users.containsKey(user.getId())) {
+    public User update(@Valid @RequestBody User user) {
+        if (users.containsKey(user.getId())) {
             users.put(user.getId(), user);
+        } else {
+            throw new ValidationException("User does not exist");
         }
         return user;
     }
@@ -40,5 +45,11 @@ public class UserController {
                 .max()
                 .orElse(0);
         return ++currentMaxId;
+    }
+
+    private void validateName(User user) {
+        if (user.getName() == null) {
+            user.setName(user.getLogin());
+        }
     }
 }
