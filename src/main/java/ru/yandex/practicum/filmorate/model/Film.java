fe73becf544f72d3faad9a2e71b80c143b759ef7
montenok <montenok-d@yandex.ru>
2024-05-09 16:08:45 +0300

    Add validation annotations

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,17 +1,24 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
-/**
- * Film.
- */
 @Data
 public class Film {
     long id;
+    @NotBlank
     String name;
+    @Size(max = 200)
     String description;
+    @NotNull
+    @DateTimeFormat
     LocalDate releaseDate;
+    @Positive
     int duration;
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,5 +1,9 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -7,8 +11,12 @@ import java.time.LocalDate;
 @Data
 public class User {
     long id;
+    @NotNull
+    @Email
     String email;
+    @NotBlank
     String login;
     String name;
+    @PastOrPresent
     LocalDate birthday;
 }
