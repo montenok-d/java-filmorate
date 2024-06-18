b91c18dc7683d404d9680aa212e65cf11d930379
montenok <montenok-d@yandex.ru>
2024-06-18 22:02:58 +0300

    Update db scheme

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,3 @@
-create TABLE IF NOT EXISTS users (
-  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  email VARCHAR(255) NOT NULL,
-  login VARCHAR(40) NOT NULL,
-  name VARCHAR(40) NULL,
-  birthday DATE NOT NULL
-);
-
 create TABLE IF NOT EXISTS mpa (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR(40) NOT NULL
@@ -14,6 +6,14 @@ create TABLE IF NOT EXISTS mpa (
 create TABLE IF NOT EXISTS genres (
    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name varchar(50) NOT NULL
+);
+
+create TABLE IF NOT EXISTS users (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  email VARCHAR(255) NOT NULL,
+  login VARCHAR(40) NOT NULL,
+  name VARCHAR(40) NULL,
+  birthday DATE NOT NULL
 );
 
 create TABLE IF NOT EXISTS films (
@@ -28,7 +28,6 @@ create TABLE IF NOT EXISTS films (
 create TABLE IF NOT EXISTS likes (
     user_id BIGINT NOT NULL REFERENCES users (id),
     film_id BIGINT NOT NULL REFERENCES films (id),
-    PRIMARY KEY (user_id, film_id)
 );
 
 create TABLE IF NOT EXISTS friends (
@@ -41,5 +40,4 @@ create TABLE IF NOT EXISTS friends (
 create TABLE IF NOT EXISTS films_genres (
     film_id  BIGINT NOT NULL REFERENCES films (id),
     genre_id BIGINT NOT NULL REFERENCES genres (id),
-    PRIMARY KEY (film_id, genre_id)
 );
