3e42786b6baade8e5022714bd62853774d1db3f9
montenok <montenok-d@yandex.ru>
2024-06-20 23:09:39 +0300

    Delete likes from Film model

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -27,7 +27,6 @@ public class Film {
     private LocalDate releaseDate;
     @Positive
     private int duration;
-    private final Set<Long> likes = new HashSet<>();
-    private final Set<Genre> genres = new HashSet<>();
+    private Set<Genre> genres = new HashSet<>();
     private Mpa mpa;
 }
