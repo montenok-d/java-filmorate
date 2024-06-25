508e90cf2507c8e3499d6908997a6dc48cc0095a
montenok <montenok-d@yandex.ru>
2024-06-26 01:18:07 +0300

    Update FilmRowMapper

--- src/main/java/ru/yandex/practicum/filmorate/storage/mappers/FilmRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/mappers/FilmRowMapper.java
@@ -4,13 +4,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.service.MpaService;
+import ru.yandex.practicum.filmorate.model.Genre;
+import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.storage.GenreDbStorage;
 import ru.yandex.practicum.filmorate.storage.MpaDbStorage;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashSet;
+import java.util.Set;
 
 @RequiredArgsConstructor
 @Component
@@ -18,18 +20,23 @@ public class FilmRowMapper implements RowMapper<Film> {
 
     private final MpaDbStorage mpaDbStorage;
     private final GenreDbStorage genreDbStorage;
-    private final MpaService mpaService;
 
     @Override
     public Film mapRow(ResultSet rs, int rowNum) throws SQLException {
+        long mpaId = rs.getLong("mpa_id");
+        String mpaName = mpaDbStorage.findMpaById(mpaId).get().getName();
+        Set<Genre> genres = new HashSet<>(genreDbStorage.findGenresByFilmId(rs.getLong("id")));
         return Film.builder()
                 .id(rs.getLong("id"))
                 .name(rs.getString("name"))
                 .description(rs.getString("description"))
                 .releaseDate(rs.getDate("release_date").toLocalDate())
                 .duration(rs.getInt("duration"))
-                .mpa(mpaService.findMpaById(rs.getInt("mpa_id")))
-                .genres(new HashSet<>(genreDbStorage.findGenresByFilmId(rs.getLong("id"))))
+                .mpa(Mpa.builder()
+                        .id(mpaId)
+                        .name(mpaName)
+                        .build())
+                .genres(genres)
                 .build();
     }
 }
