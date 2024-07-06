9bbc8b9c6e2f1a088d462069b23f8b8592eac55e
Vichy <andrbowname@yandex.ru>
2024-07-06 15:10:09 -0300

    add-feed

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -10,7 +10,6 @@ import ru.yandex.practicum.filmorate.storage.UserStorage;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Set;
 
 @Service
 public class UserService {
