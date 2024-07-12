637b3e7f14c75c88f2de5d08c0d9dbb7eea2d25e
montenok <montenok-d@yandex.ru>
2024-07-12 15:33:28 +0300

    FilmRowMapper fix

--- src/main/java/ru/yandex/practicum/filmorate/mapper/FilmRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/mapper/FilmRowMapper.java
@@ -5,8 +5,6 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Mpa;
-import ru.yandex.practicum.filmorate.storage.director.DirectorStorage;
-import ru.yandex.practicum.filmorate.storage.genre.GenreStorage;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -14,9 +12,6 @@ import java.sql.SQLException;
 @RequiredArgsConstructor
 @Component
 public class FilmRowMapper implements RowMapper<Film> {
-
-    private final GenreStorage genreStorage;
-    private final DirectorStorage directorStorage;
 
     @Override
     public Film mapRow(ResultSet rs, int rowNum) throws SQLException {
